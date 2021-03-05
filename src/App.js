445d41a8837a9a13f9f0a5553a5fdb4eff358726
vinayvadlamuri <vinayvadlamuri@gmail.com>
2021-03-06 01:52:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Vinay Vadlamurii
           </a>
         </header>
       </div>
